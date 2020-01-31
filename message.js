69d9593f97b275c14d46ed9a3b4cb6cf907499f7
tetrew88 <45017955+tetrew88@users.noreply.github.com>
2020-02-01 00:32:41 +0100

    add post method action

---
+++ message.js
@@ -0,0 +1,66 @@
+function post_message(senderName, message)
+{
+	const chatZone = document.getElementById('conversation_screen');
+
+	const newElement = document.createElement('li');
+
+	newElement.classList.add("bubble_text");
+	newElement.textContent = senderName + ": " + message;
+
+	if(senderName == "grandPy")
+	{
+		newElement.classList.add("start_allign");	
+	}
+	else
+	{
+		newElement.classList.add("end_allign");
+		newElement.style.backgroundColor = "green";
+	}
+
+	chatZone.appendChild(newElement);
+
+	chatZone.scrollTop = chatZone.scrollHeight;
+}
+
+function collect_message()
+{
+	const userInput = document.getElementById("userText");
+
+	text = userInput.value;
+
+	if(text == "")
+	{
+		alert("Entrer une question");
+	}
+	else
+	{
+		post_message("vous", text);
+	}
+
+	fetch('/ask',
+	{
+		method: "POST",
+		body: text,
+		headers:
+		{
+			'Content-Type': 'text/plain'
+		}
+	}).then(response => {
+		if (response.ok)
+		{
+			response.text()
+            .then(console.log)
+            .catch(error =>
+            {
+                console.error(error);
+            });
+    	}
+    	else 
+    	{
+    		console.error('server response : ' + response.status);
+    	}
+    }).catch(error =>
+    {
+    	console.error(error);
+	});
+}
